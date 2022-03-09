4a76265eb525c796bae3d62ce887bbf83866858e
Denis Dedkov <dephyys@gmail.com>
2022-03-10 01:11:53 +0300

    Add strcpy and itoa
    These will be helper functions for printf():
    
    strcmp        - copies null-terminated string
    		to the buffer provided
    
    itoa10/itoa2x - converts integer to a string

--- printf.s
+++ printf.s
@@ -2,6 +2,107 @@ global  _start
 
 section .text
 
+;------------------------------------------------
+; Copies null-terminated string from one buffer
+; to another.
+;
+; Expect: rsi - null terminated string copy from
+;	  rdi - buffer copy to
+;
+; Destrs: rax rsi rdi
+;
+; Important! Null-terminator is not copied. 
+;------------------------------------------------
+strcpy:
+	cld
+	xor rax, rax		
+.copy:	
+	lodsb		
+	test al, al
+	jz .stop
+			
+	stosb	
+	jmp .copy
+	
+.stop:
+	ret
+;------------------------------------------------
+
+;------------------------------------------------
+; Converts an integer value to a string using 
+; the base of 10.
+;
+; Expect: rax - integer to convert
+;	  rdi - buffer to store string
+;
+; Destrs: rax rbx rdx rdi  
+;------------------------------------------------
+itoa10:
+	cld
+	push rbp
+	mov rbp, rsp	
+	mov rbx, 10	
+
+.save_dgt:	
+	xor rdx, rdx	; Push remainder	
+ 	div rbx		; to stack
+	add rdx, '0'	;
+ 	push rdx	;
+ 	
+ 	test rax, rax
+	jnz .save_dgt	
+	
+.reverse:	
+	pop rax		; Store digit characters in	
+	stosb		; reversed order
+	cmp rbp, rsp	;
+	jnz .reverse	;
+
+	pop rbp
+	ret
+;------------------------------------------------
+
+;------------------------------------------------
+; Converts an integer value to a string using 
+; the base of the power of 2.
+;
+; Expect: 
+;	  rax - integer to convert
+;	   cl - base (power of 2)
+;	  rbx - mask 
+;	  rdi - buffer to store string
+;
+; Destrs: rax rbx rdx rdi  
+;------------------------------------------------
+itoa2x:
+	cld
+	push rbp
+	mov rbp, rsp	
+	
+.save_dgt:	 
+	mov rdx, rax			; Push remainder to the stack	
+ 	and rdx, rbx			;
+	movzx rdx, byte [xlattab + rdx]	;
+ 	push rdx			;
+ 	shr rax, cl 			;
+ 	
+ 	test rax, rax
+	jnz .save_dgt	
+	
+.reverse:	
+	pop rax				; Store digit characters in	
+	stosb				; reversed order
+	cmp rbp, rsp			;
+	jnz .reverse			;
+
+	pop rbp
+	ret
+
+section .data
+xlattab db '0123456789abcdef'
+section .text
+;------------------------------------------------
+
 
 puts:	
 	mov     rdi, 1                 
@@ -13,6 +114,15 @@ printf:
 	push 	rbp
 	mov 	rbp, rsp
 
+	mov rsi, message
+	mov rdi, printf_buf
+	
+	mov rax, 13
+	mov cl, 1
+	mov rbx, 1
+	
+	call itoa2x	
+	
 	mov 	rdi, [rbp + 16]
 	xor 	rax, rax
 
@@ -42,6 +152,7 @@ printf:
 	
 	cmp	dl,  '%'
 	je 	.pct
+
 	
 	lea 	rdx, [rdx - 'b']
 	cmp	rdx, 'x' - 'b'
